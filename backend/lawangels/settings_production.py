04674a3822c7dba271215a344f038c63917b7ddf
Posiislight <adelekeolamiposi@gmail.com>
2025-11-24 05:01:05 +0100

    Fix: Remove gunicorn config from Procfile and fix middleware import paths

--- backend/lawangels/settings_production.py
+++ backend/lawangels/settings_production.py
@@ -205,7 +205,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'middleware.RequestTimingMiddleware',
     'logging_utils.DetailedRequestLoggingMiddleware',
 ]
