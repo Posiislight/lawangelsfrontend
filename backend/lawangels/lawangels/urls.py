a95e5e974387be054eb2cbe80bdd8916a0467362
Posiislight <adelekeolamiposi@gmail.com>
2025-11-24 02:31:43 +0100

    a

--- backend/lawangels/lawangels/urls.py
+++ backend/lawangels/lawangels/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('quiz.urls')),
-    path('api/', include('auth_app.urls')),
+    path('api/', include('lawangels.quiz.urls')),
+    path('api/', include('lawangels.auth_app.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
