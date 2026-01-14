9e40b2ab4e2b6b0ac783d8fe9ed267826ea06b47
Posiislight <adelekeolamiposi@gmail.com>
2026-01-14 02:57:42 +0100

    Re-enable console logging for production

--- lawangels/vite.config.ts
+++ lawangels/vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    // Remove console.log and debugger statements in production
-    drop: process.env.NODE_ENV === 'production' ? ['console', 'debugger'] : [],
-  },
 })
